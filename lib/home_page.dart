54b13075af1e5f2c1651f27d3996ae9677e72dab
NanoNish <nishkarsh1206@gmail.com>
2023-04-06 01:54:40 +0530

    feat: add qr generator for broadcast

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:ping_discover_network_forked/ping_discover_network_forked.dart';
+// import 'package:ping_discover_network_forked/ping_discover_network_forked.dart';
 import 'package:network_info_plus/network_info_plus.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+// import 'package:bonsoir/bonsoir.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,16 +12,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? ip;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<String> getIP() async {
+    final info = NetworkInfo();
+    String? wifiIP = await info.getWifiIP();
+    return wifiIP!;
+  }
+
+  void scan() {}
+
+  void broadcast() async {
+    final tempIP = await getIP();
+    setState(() {
+      ip = tempIP;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         const SizedBox(
-          height: 200,
-        ),
-        Container(
-          color: Colors.black54,
+          height: 100,
         ),
         Container(
           decoration: BoxDecoration(
@@ -27,35 +49,41 @@ class _HomePageState extends State<HomePage> {
               color: Colors.black,
             ),
           ),
-          child: const TextButton(
-            onPressed: onPressed,
-            child: Text("Scan"),
+          child: TextButton(
+            onPressed: scan,
+            child: const Text("Scan"),
           ),
         ),
+        const SizedBox(
+          height: 100,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+            ),
+          ),
+          child: TextButton(
+            onPressed: broadcast,
+            child: const Text("Broadcast"),
+          ),
+        ),
+        Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+              ),
+            ),
+            child: (ip == null
+                ? const SizedBox(
+                    height: 0,
+                  )
+                : QrImage(
+                    data: ip!,
+                    version: QrVersions.auto,
+                    size: 200.0,
+                  ))),
       ],
     ));
   }
-}
-
-void onPressed() async {
-  final info = NetworkInfo();
-  final wifiIP = await info.getWifiIP();
-  print(wifiIP);
-  const port = 443;
-  final stream = NetworkAnalyzer.discover2(
-    '110.61.5',
-    port,
-    timeout: const Duration(milliseconds: 5000),
-  );
-
-  int found = 0;
-  stream.listen((NetworkAddress addr) {
-    if (addr.exists) {
-      found++;
-      print('-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^');
-      print('-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^');
-      print('-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^');
-      print('Found device: ${addr.ip}:$port');
-    }
-  });
 }
